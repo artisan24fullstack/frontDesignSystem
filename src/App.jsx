50d3e90adec5574d7253688011cbe36d3574c9b7
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-06-04 10:09:25 +0200

    mise en place

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Design system
+      </h1>
+      <p>with Tailwind.css, Vite & React.js</p>
+    </>
+  )
+}
+
+export default App
